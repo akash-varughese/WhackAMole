bad245e2629d6e17d3f5ec66c48a9fc1f16aa6af
Akash Varughese <akash.varughese.284@k12.friscoisd.org>
2020-10-27 14:03:17 -0500

    Set game board

--- WhackAMole/ViewController.swift
+++ WhackAMole/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var btn = UIButton()
+    var screenWidth = 0
+    var screenHeight = 0
+    var scoreBox = UILabel()
+    var background = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let screenBounds: CGRect = UIScreen.main.bounds
+        screenWidth = Int(screenBounds.width)
+        screenHeight = Int(screenBounds.height)
+        let scoreBox = UILabel(frame: CGRect(x: 20, y: 30, width: 200, height: screenHeight/10))
+        scoreBox.text = "0"
+        scoreBox.center = CGPoint(x: 20, y: 30)
+        scoreBox.textAlignment = .center
+        self.view.addSubview(scoreBox)
+        let background = UILabel(frame: CGRect(x: 20, y: 40, width: screenWidth-40, height: screenHeight-60))
+        background.backgroundColor = .green
+        self.view.addSubview(background)
     }
-
-
 }
-
